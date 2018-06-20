38b8f6a867f5c264cf103580a1b2d9ec90d92aff
andreydro <andreydro14@gmail.com>
2018-06-20 23:14:32 +0200

    Update 2 chapter

---
+++ ruby_primer_ascent/2_chapter_tasks.rb
@@ -0,0 +1,134 @@
+# 2.0 Instance Variables and Accessors
+
+class Item
+  def initialize(description, color)
+    @description = description
+    @color = color
+  end
+  
+  def description
+    @description = @color
+  end
+end
+
+###
+
+class Item
+  def initialize(item_name, quantity)
+    @item_name = item_name
+    @quantity = quantity
+  end
+  
+  def quantity=(new_quantity)
+    @quantity = new_quantity
+  end
+  
+  def quantity
+    @quantity
+  end  
+end
+
+item = Item.new("a",1)
+item.quantity = 3
+p item.quantity
+
+# 2.1 Class Variables and Methods
+
+class ApplicationConfiguration
+  def self.set(property_name, value)
+    @property_name = property_name
+    @value = value
+  end
+  
+  def self.get(property_name)
+    @value
+  end
+end
+
+ApplicationConfiguration.set("name", "Demo Application")
+ApplicationConfiguration.set("version", "0.1")
+
+p ApplicationConfiguration.get("version")
+
+###
+
+class ApplicationConfiguration
+  @configuration = {}
+
+  def self.set(property, value)
+    @configuration[property] = value
+  end
+
+  def self.get(property)
+    @configuration[property]
+  end
+end
+
+class ERPApplicationConfiguration < ApplicationConfiguration
+  @configuration = {}
+end
+
+class WebApplicationConfiguration < ApplicationConfiguration
+  @configuration = {}
+end
+
+ERPApplicationConfiguration.set("name", "ERP Application")
+WebApplicationConfiguration.set("name", "Web Application")
+
+p ERPApplicationConfiguration.get("name")
+p WebApplicationConfiguration.get("name")
+
+p ApplicationConfiguration.get("name")
+
+# Equality of Objects
+
+class Item
+    attr_reader :item_name, :qty
+    
+    def initialize(item_name, qty)
+        @item_name = item_name
+        @qty = qty
+    end
+    def to_s
+        "Item (#{@item_name}, #{@qty})"
+    end
+    def ==(other_item)
+      if other_item.item_name == @item_name && other_item.qty == @qty
+        true
+      end
+    end
+end
+
+p Item.new("abcd",1)  == Item.new("abcd",1)
+p Item.new("abcd",2)  == Item.new("abcd",1)
+
+###
+
+class Item
+  attr_reader :item_name, :quantity, :supplier_name, :price
+  
+  def initialize(item_name, quantity, supplier_name, price)
+    @item_name = item_name
+    @quantity = quantity
+    @supplier_name = supplier_name
+    @price = price
+  end 
+  
+  def ==(other_item)
+    self.eql?(other_item)
+  end
+  
+  def eql?(other_item)
+    @item_name == other_item.item_name &&
+    @quantity == other_item.quantity &&
+    @supplier_name == other_item.supplier_name &&
+    @price == other_item.price
+  end
+  
+  def hash
+    @item_name.hash ^ @quantity.hash ^ @supplier_name.hash ^ @price.hash
+  end
+end
+
+# 2.3 Displaying Objcects: puts and p, to_s and inspect
+
